2a025fcc58b03e6ba46c8be8b589f6af0612e00f
PriontoAbdullah <priontosoft@gmail.com>
2021-02-16 14:16:52 +0600

    All actions dispatch in Cart complete

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,106 @@
+import currencyFormatter from 'currency-formatter';
 import React from 'react';
+import { BsDash, BsPlus } from 'react-icons/bs';
+import { MdRemoveShoppingCart } from 'react-icons/md';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>Cart</h1>
-        </div>
-    );
+	const { products } = useSelector((state) => state.CartReducer);
+	const dispatch = useDispatch();
+
+	const handleSizeChange = (status, id) => {
+		dispatch({ type: 'SIZE', payload: {status, id} });
+	};
+
+	return (
+		<div className="cart mx-5">
+			<h3>Your Cart</h3>
+			{products.length > 0 ? (
+				<div className="row">
+					<div className="col-md-9 mb-4">
+						<div className="cart-heading">
+							<div className="row">
+								<div className="col-md-2">Picture </div>
+								<div className="col-md-3">Name </div>
+								<div className="col-md-1">Price </div>
+								<div className="col-md-2 text-center">Quantity </div>
+								<div className="col-md-1">Size</div>
+								<div className="col-md-2 text-center">Total Price</div>
+								<div className="col-md-1">Remove </div>
+							</div>
+						</div>
+						{products.map((product) => (
+							<div className="row mt-2 align-items-center" key={product.id}>
+								<div className="col-md-2">
+									<div className="cart-image">
+										<img src={`/images/${product.image}`} alt="product" />
+									</div>
+								</div>
+								<div className="col-md-3">
+									<div className="cart-name">{product.name}</div>
+								</div>
+								<div className="col-md-1 ">
+									<div className="cart-price">
+										{currencyFormatter.format(product.discountPrice, { code: 'USD' })}
+									</div>
+								</div>
+
+								<div className="col-md-2">
+									<div className="details-info ">
+										<div className="details-incDec">
+											<span
+												className="dec-cart"
+												onClick={() => dispatch({ type: 'DEC', payload: product.id })}
+											>
+												<BsDash />
+											</span>
+											<span className="quantity-cart">{product.quantity}</span>
+											<span
+												className="inc-cart"
+												onClick={() => dispatch({ type: 'INC', payload: product.id })}
+											>
+												<BsPlus />
+											</span>
+										</div>
+									</div>
+								</div>
+								<div className="col-md-1">
+									<select
+										onChange={(e) => handleSizeChange(e.target.value, product.id)}
+										defaultValue={product.size}
+									>
+										<option>S</option>
+										<option>M</option>
+										<option>L</option>
+										<option>XL</option>
+									</select>
+								</div>
+
+								<div className="col-md-2 pl-4">
+									<div className="cart-total">
+										{currencyFormatter.format(product.discountPrice * product.quantity, {
+											code: 'USD'
+										})}
+									</div>
+								</div>
+								<div className="col-md-1">
+									<div
+										className="cart-remove"
+										onClick={() => dispatch({ type: 'REMOVE', payload: product.id })}
+									>
+										<MdRemoveShoppingCart />
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className="col-md-3">Summery</div>
+				</div>
+			) : (
+				'Your cart is empty!'
+			)}
+		</div>
+	);
 };
 
 export default Cart;
--- src/store/reducers/CartReducer.js
+++ src/store/reducers/CartReducer.js
@@ -5,6 +5,7 @@ const initState = {
 };
 
 const CartReducer = (state = initState, action) => {
+
 	switch (action.type) {
 		case 'ADD_TO_CART':
 			const { product, quantity, size } = action.payload;
@@ -24,6 +25,59 @@ const CartReducer = (state = initState, action) => {
 					totalPrice: TotalPrice,
 					totalQuantity: TotalQuantity
 				};
+			}
+
+		case 'INC':
+			const findIncProduct = state.products.find((product) => product.id === action.payload);
+			const findIncProductIndex = state.products.findIndex((product) => product.id === action.payload);
+
+			findIncProduct.quantity += 1;
+			state.products[findIncProductIndex] = findIncProduct;
+
+			return {
+				...state,
+				totalPrice: state.totalPrice + findIncProduct.discountPrice,
+				totalQuantity: state.totalQuantity + 1
+			};
+
+		case 'DEC':
+			const findDexProduct = state.products.find((product) => product.id === action.payload);
+			const findDecProductIndex = state.products.findIndex((product) => product.id === action.payload);
+
+			if (findDexProduct.quantity > 1) {
+				findDexProduct.quantity -= 1;
+				state.products[findDecProductIndex] = findDexProduct;
+
+				return {
+					...state,
+					totalPrice: state.totalPrice - findDexProduct.discountPrice,
+					totalQuantity: state.totalQuantity - 1
+				};
+			} else {
+				return state;
+			}
+
+		case 'SIZE':
+			const findSizeProduct = state.products.find((product) => product.id === action.payload.id);
+			const findSizeProductIndex = state.products.findIndex((product) => product.id === action.payload.id);
+			console.log(action.payload.id);
+			console.log(action.payload.status);
+			findSizeProduct.size = action.payload.status;
+			state.products[findSizeProductIndex] = findSizeProduct;
+
+			return {
+				...state
+			};
+
+		case 'REMOVE':
+			const findRemoveProduct = state.products.find((product) => product.id === action.payload);
+			const filterProduct = state.products.filter((product) => product.id !== action.payload);
+
+			return {
+				...state,
+				products: filterProduct, 
+				totalPrice: state.totalPrice - findRemoveProduct.discountPrice * findRemoveProduct.quantity, 
+				totalQuantity: state.totalQuantity - findRemoveProduct.quantity
 			}
 
 		default:
